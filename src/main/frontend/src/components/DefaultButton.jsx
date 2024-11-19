2e1f46d0513f9f4bd74e880c2496909da128606a
KOSMO <kwon11184@gmail.com>
2024-11-19 19:01:16 +0900

    https://github.com/poqazpoqaz/pitchplay/tree/master/src/main/frontend

---
+++ src/main/frontend/src/components/DefaultButton.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const DefaultButton = ({ children }) => {
+
+  const buttonStyle = {
+    backgroundColor: '#07550C', 
+    color: 'white',            
+    border: 'none',                 
+    padding: '12px 24px',     
+    fontSize: '16px',          
+    borderRadius: '8px',        
+  };
+
+  // const hoverStyle = {
+   //추후 호버효과를 위해 주석처리를 해놨습니다.
+  // };
+
+  return (
+    <button
+      style={buttonStyle}
+      // onMouseEnter={(e) => { e.target.style = { ...buttonStyle, ...hoverStyle }; }} 
+      // onMouseLeave={(e) => { e.target.style = buttonStyle; }} 
+      // 추후 hover효과를 위해 주석처리를해놨습니다
+    >
+      {children}
+    </button>
+  );
+};
+
+export default DefaultButton;
