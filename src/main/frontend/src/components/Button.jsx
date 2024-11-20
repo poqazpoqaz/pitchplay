128976c1bd588012d07b0995c246334b9922a50c
coding-ji <eunjikwon0530@hotmail.com>
2024-11-20 09:56:27 +0900

    Button업데이트

--- src/main/frontend/src/components/Button.jsx
+++ src/main/frontend/src/components/Button.jsx
@@ -1,21 +1,28 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 // StyledButton 컴포넌트 정의
-    const StyledButton = styled.button`
-        background: ${props => props.color ? props.color: '#FFFFFF'};  // color가 없으면 기본 흰색
-        color: ${props => props.color ? '#FFFFFF' : '#000000'}; // background color가 있으면 흰색, 없으면 검정색
+const StyledButton = styled(motion.button)`
+        background: '#FFFFFF';  // color가 없으면 기본 흰색
+        color: ${props => props.color ? '#000000' : '#FFFFFF'}; // background color가 있으면 흰색, 없으면 검정색
         font-weight: bold;
-        margin: 20px 30px;
+        margin: 20px 30px; 
         padding: 10px 20px;
         border-radius: 15px;
         border: 2px solid var(--main-color);
+
+        &:hover{
+            background: ${props=> props.color};  // color가 없으면 기본 흰색
+            color: #ffffff; // background color가 있으면 흰색, 없으면 검정색
+        }
     `;
 
-function Button({ color, text }) {
+function Button({ color, children }) {
     return (
-        // StyledButton에 color와 text를 props로 전달
-        <StyledButton color={color}>{text}</StyledButton>
+        <StyledButton color={color}>{children}</StyledButton>
     );
 }
 
 export default Button;
+
+// 사용방법 : <Button color='var(--main-color)'>안녕</Button>
