b84140fc4fd4a7f5a44983f70b6a309a430bb457
kosmo <iamsang1004@gmail.com>
2024-11-25 09:47:59 +0900

    마이페이지 - 사이드바(스타일트 컴포넌트로 지정)

--- src/main/frontend/src/containers/mypageSidebar/Sidebar.jsx
+++ src/main/frontend/src/containers/mypageSidebar/Sidebar.jsx
@@ -1,67 +1,161 @@
 import React from 'react';
-import './Sidebar.css';
+import styled from 'styled-components';
 import pachiImage from './pachi.jpg';
 import axios from 'axios';
-import myinfo from "/imgs/myinfo.svg";
-import myactivity from "/imgs/myactivity.svg";
-import commnuity from "/imgs/commnuity.svg";
-import setting from "/imgs/setting.svg";
+import myinfo from '/imgs/myinfo.svg';
+import myactivity from '/imgs/myactivity.svg';
+import commnuity from '/imgs/commnuity.svg';
+import setting from '/imgs/setting.svg';
 
+// Styled Components
+const SidebarContainer = styled.div`
+  display: grid;
+  height: 100%;
+  border-right: 1px solid #efefef;
+  grid-template:
+    "... ... ..." 1fr
+    "... sidetop ..." 90px
+    "... ... ..." 0.3fr
+    "... sidemid ..." 240px
+    "... ... ..." 1fr
+    "... sidebottom ..." 50px
+    "... ... ..." 1fr / 10px 240px 10px;
+`;
+
+const Sidetop = styled.div`
+  grid-area: sidetop;
+  display: flex;
+  gap: 1rem;
+  border: 1px solid #efefef;
+  border-radius: 15%;
+`;
+
+const Sidemid = styled.div`
+  grid-area: sidemid;
+`;
+
+const Sidebottom = styled.div`
+  grid-area: sidebottom;
+`;
+
+const UserInfo = styled.div`
+  .username {
+    font-size: 1rem;
+    font-weight: bold;
+  }
+  .useremail {
+    font-size: 0.75rem;
+  }
+`;
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+const ListItem = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  transition: background-color 0.3s ease;
+
+
+  &:hover {
+    background-color: #f0f0f0;
+  }
+`;
+
+const Button = styled.span`
+  padding: 1rem;
+  color: red;
+  cursor: pointer;
+`;
+
+const TopImg = styled.img`
+  width: 4rem;
+  height: 4rem;
+  border-radius: 100%;
+  margin-top: 1rem;
+  margin-left: 1rem;
+`;
+
+const BotImg = styled.img`
+  width: 3rem;
+  height: 3rem;
+  border-radius: 100%;
+`;
+const Link = styled.a`
+  text-decoration: none;
+  font-size: 0.875rem;
+  color: #333;
+  transition: color 0.3s ease;
+  &:hover {
+    color: #007bff;
+  }
+`;
+
+
+// Sidebar Component
 const Sidebar = ({ avatar, userName, userEmail }) => {
 
-    const handleLogout = async () => {
-        try {
-            // 로그아웃 요청을 서버에 보냄
-            const response = await axios.post('/api/logout');  // 로그아웃 API 엔드포인트
+  const handleLogout = async () => {
+    try {
+      // 로그아웃 요청을 서버에 보냄
+      const response = await axios.post('/api/logout');  // 로그아웃 API 엔드포인트
 
-            if (response.status === 200) {
-                // actions 랑 연동될 유지
-                // 로그인 페이지로 이동
-            }
-        } catch (error) {
-            console.error('로그아웃 실패:', error);
-            alert('로그아웃 중 오류가 발생했습니다. 다시 시도해주세요.');
-        }
-    };
-    return (
+      if (response.status === 200) {
+        // actions 랑 연동될 유지
+        // 로그인 페이지로 이동
+      }
+    } catch (error) {
+      console.error('로그아웃 실패:', error);
+      alert('로그아웃 중 오류가 발생했습니다. 다시 시도해주세요.');
+    }
+  };
 
-        <div className="sidebar">
-            <div className="sidetop">
-                {/* 이미지는 프롭스로 받아올꺼셈 */}
-                {/* <img className="avatar" src={avatar} alt="User Avatar" /> */}
-                <img className="topimg" src={pachiImage} alt="" />
-                <div className="userinfo">
-                    {/* 네임이랑 이메일도 프롭스로 받아올꺼셈 임시로 넣어둠 */}
-                    <p className="username">{userName}박상진</p>
-                    <p className="useremail">{userEmail}kosmo147@kosmo.com</p>
-                </div>
-            </div>
-            <div className='sidemid' >
-                <ul>
-                    <li>
-                        <img src={myinfo} alt="" />
-                        <a href="">내 정보</a></li>
-                    <li>
-                        <img src={myactivity} alt="" />
-                        <a href="">내 활동</a></li>
-                    <li>
-                        <img src={commnuity} alt="" />
-                        <a href="">커뮤니티</a></li>
-                    <li>
-                        <img src={setting} alt="" />
-                        <a href="">설정</a></li>
-                </ul>
-            </div>
-            <div className="sidebottom">
-                {/* 이미지는 프롭스로 받아올꺼셈 */}
-                {/* <img className="avatar_bottom" src={avatar} alt="User Avatar" /> */}
-                <img className='botimg' src={pachiImage} alt="" />
-                <span className="logout" onClick={handleLogout}>로그아웃</span>
-            </div>
-        </div>
-
-
-    )
+  return (
+    <SidebarContainer>
+      <Sidetop>
+        <TopImg src={pachiImage} alt="User Avatar" />
+        <UserInfo>
+          <p className="username">{userName}박상진</p>
+          <p className="useremail">{userEmail}kosmo147@kosmo.com</p>
+        </UserInfo>
+      </Sidetop>
+      <Sidemid>
+        <List>
+          <ListItem>
+            <img src={myinfo} alt="내 정보" />
+            {/* Link 컴포넌트로 변경 */}
+            <Link href="">내 정보</Link>
+          </ListItem>
+          <ListItem>
+            <img src={myactivity} alt="내 활동" />
+            {/* Link 컴포넌트로 변경 */}
+            <Link href="">내 활동</Link>
+          </ListItem>
+          <ListItem>
+            <img src={commnuity} alt="커뮤니티" />
+            {/* Link 컴포넌트로 변경 */}
+            <Link href="">커뮤니티</Link>
+          </ListItem>
+          <ListItem>
+            <img src={setting} alt="설정" />
+            {/* Link 컴포넌트로 변경 */}
+            <Link href="">설정</Link>
+          </ListItem>
+        </List>
+      </Sidemid>
+      <Sidebottom>
+        <BotImg src={pachiImage} alt="User Avatar" />
+        <Button onClick={handleLogout}>로그아웃</Button>
+      </Sidebottom>
+    </SidebarContainer>
+  );
 };
 
 export default Sidebar;
