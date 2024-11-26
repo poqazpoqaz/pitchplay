bd96b9a7b46cd3deb348133ee18f9a6efe713b41
kosmo <tmddn7439@naver.com>
2024-11-26 17:53:55 +0900

    구장 상세정보 페이지

--- src/main/frontend/src/components/FieldCard.jsx
+++ src/main/frontend/src/components/FieldCard.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';  // Link 임포트
 import './FieldCard.css';  // CSS 파일을 불러옵니다.
 
-function FieldCard({ image, name, address, type, area }) {
+function FieldCard({ IMGURL, SVCNM, PLACENM, PAYATNM, TELNO, SVCID }) {
   return (
     <div className="card">
-      <img src={image} alt={name} />
+      <img src={IMGURL} alt={SVCNM} className="card-image" />
       <div className="info-container">
         <div className="info-box">
-          <div className="info-text">{name}</div>
-          <div className="info-text">{address}</div>
-          <div className="info-text">{type}</div>
-          <div className="info-text">{area}</div>
-          <button className="button">상세보기</button>
+          <div className="info-text"><strong>시설명:</strong> {SVCNM}</div>
+          <div className="info-text"><strong>위치:</strong> {PLACENM}</div>
+          <div className="info-text"><strong>요금:</strong> {PAYATNM}</div>
+          <div className="info-text"><strong>전화:</strong> {TELNO}</div>
+          {/* Link로 상세보기 버튼을 변경 */}
+          <Link to={`/stadium/${SVCID}`} className="button">상세보기</Link>
         </div>
       </div>
     </div>
