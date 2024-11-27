5f4246515fb820d813a574cb394e2ac283af8b70
kosmo <iamsang1004@gmail.com>
2024-11-27 18:20:44 +0900

    상진 - 모듈 완성?

--- src/main/frontend/src/pages/Mypage/MypageAct/MypageAct.jsx
+++ src/main/frontend/src/pages/Mypage/MypageAct/MypageAct.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import Sidebar from "./Sidebar";
-import MatchesSection from "./MypageActComponents/MatchesSection";
-import RecordsSection from "./MypageActComponents/RecordsSection";
-import TeamSection from "./MypageActComponents/TeamSection";
-import PostsSection from "./MypageActComponents/PostsSection";
-import { useStore } from '../../../stores/MypageStore/useStore';  // store 훅 임포트
+import MatchesSection from "./MatchesSection";
+import RecordsSection from "./RecordsSection";
+import TeamSection from "./TeamSection";
+import PostsSection from "./PostsSection";
+import { useStore } from '../../../stores/useStore';  // store 훅 임포트
 import styles from "./MypageAct.module.css";
+
 
 const MypageAct = () => {
   const { state, actions } = useStore();  // 스토어에서 상태와 액션을 가져옵니다.
   const { matches, records, teamInfo, posts } = state;  // 필요한 데이터 가져오기
 
+
+
   return (
-    <div className={styles.mypageGrid}>
-      <div className={styles.container}>
-        <Sidebar className={styles.sidebar} />
+    <div>
+      <div>
         <div className={styles.content}>
-          <h1 className={styles.title}>마이페이지 &gt; 내 활동</h1>
+          <h1>마이페이지 &gt; 내 활동</h1>
           <MatchesSection matches={matches} />
           <RecordsSection records={records} />
           <TeamSection teamInfo={teamInfo} />
